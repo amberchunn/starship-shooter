c7d5c0864af0bb7d0006a124d298466b297bfd0f
Amber Chunn <a.77@me.com>
2022-06-01 15:04:34 -0400

    Added Player Boundaries, Wrapped Player Position to Keep in Bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,7 +17,12 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+        float currentPlayerX = transform.position.x;
+        float currentPlayerY = transform.position.y;
+        float xBoundLeft = -11.3f;
+        float xBoundRight = 8.7f;
+        float yBoundTop = 4.5f;
+        float yBoundBottom = -5.0f;
         // transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         // transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
@@ -25,5 +30,21 @@ public class Player : MonoBehaviour
         // transform.Translate(direction * _speed * Time.deltaTime);
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) *_speed * Time.deltaTime);
+        // Wrap player position whenever they hit a boundary
+        // y bound = 4.5 top, -5.0 bottom
+
+        if (currentPlayerY > yBoundTop) {
+            transform.position = new Vector3(currentPlayerX, yBoundBottom, 0);
+        } else if (currentPlayerY < yBoundBottom) {
+            transform.position = new Vector3(currentPlayerX, yBoundTop, 0);
+        }
+
+        // x bound = -11.3 left, 8.7 right
+        if (currentPlayerX < xBoundLeft) {
+            transform.position = new Vector3 (xBoundRight, currentPlayerY, 0);
+        } else if (currentPlayerX > xBoundRight) {
+            transform.position = new Vector3 (xBoundLeft, currentPlayerY, 0);
+        }
+
     }
 }
